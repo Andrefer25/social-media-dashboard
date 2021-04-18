263c10f79cd720f64129e34dcd66d5613c43c68c
Andrefer25 <andrefer2505@gmail.com>
2021-04-18 19:43:42 -0300

    finished

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './header';
 import Metrics from './metrics';
+import Overview from './overview';
+import { setTheme } from './../../utils/themes';
 
-const Dashboard = () => {
-    return(
-        <>
-            <Header />
-            <Metrics />
-        </>
-    )
+class Dashboard extends Component {
+
+    componentDidMount() {
+        if (localStorage.getItem('theme') === 'light-theme') {
+            setTheme('light-theme');
+        } else {
+            setTheme('dark-theme');
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <Header />
+                <Metrics />
+                <Overview />
+            </>
+        )
+    }
 }
 
 export default Dashboard;
